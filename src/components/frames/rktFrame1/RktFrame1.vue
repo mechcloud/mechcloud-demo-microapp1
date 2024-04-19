8893911795fc546b963ae7314048dd58c003e026
Shailendra Singh <shailendra.singh@mechcloud.io>
2024-04-19 11:56:34 +0530

    Added styles for header/main tags.

--- src/components/frames/rktFrame1/RktFrame1.vue
+++ src/components/frames/rktFrame1/RktFrame1.vue
@@ -12,3 +12,23 @@ import {
    McRenderNode
 } from '@mechcloud/piston-ui-sdk'
 </script>
+
+<style scoped>
+header {
+   padding-inline-start: 1rem;
+    padding-inline-end: 1rem;
+    padding-top: .75rem;
+    padding-bottom: .75rem;
+    background-color: #f9fafb;
+    border-bottom: 1px solid #f3f4f6;
+    z-index: 5;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
+main {
+   padding: 1rem;
+}
+</style>
