e41fb4ddff4ac621e512d9bbb3a1d5d0ed8dfa28
Shailendra Singh <shailendra.singh@mechcloud.io>
2024-04-19 12:55:49 +0530

    Added top navigation in RktFrame1.

--- src/components/frames/rktFrame1/RktFrame1.vue
+++ src/components/frames/rktFrame1/RktFrame1.vue
@@ -1,6 +1,18 @@
 <template>
-   <header>
-      <strong style="font-size: larger;">Rocket</strong>
+   <header class="mc-flex mc-flex-h-space-between">
+      <span>
+         <strong style="font-size: larger;">Rocket</strong>
+      </span>
+      <ul>
+         <li v-for="siteNode in $mcContext.siteMap">
+            <mc-navigation-link 
+               :title="siteNode.attrs.title"
+               uriPrefix=""
+               :mapping="siteNode.attrs.mapping" 
+            />
+         </li>
+      </ul>
+      <span class="email">{{ $mcContext.user.email }}</span>
    </header>
    <main>
       <mc-render-node />
@@ -9,23 +21,43 @@
 
 <script setup>
 import { 
-   McRenderNode
+   McRenderNode,
+   McNavigationLink
 } from '@mechcloud/piston-ui-sdk'
 </script>
 
 <style scoped>
 header {
    padding-inline-start: 1rem;
-    padding-inline-end: 1rem;
-    padding-top: .75rem;
-    padding-bottom: .75rem;
-    background-color: #f9fafb;
-    border-bottom: 1px solid #f3f4f6;
-    z-index: 5;
-    position: sticky;
-    top: 0;
-    left: 0;
-    right: 0;
+   padding-inline-end: 1rem;
+   padding-top: .75rem;
+   padding-bottom: .75rem;
+   background-color: #f9fafb;
+   border-bottom: 1px solid #f3f4f6;
+   z-index: 5;
+   position: sticky;
+   top: 0;
+   left: 0;
+   right: 0;
+
+   & > ul {
+      list-style-type: none;
+      display: inline-flex;
+      gap: 1rem;
+      margin: 0;
+
+      & a {
+         text-decoration: none;
+         color: black;
+         font-weight: 500;
+         font-size: 0.825rem;
+      }
+   }
+
+   & > .email {
+      color: black;
+      font-weight: 500;
+   }
 }
 
 main {
