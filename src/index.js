511c91e5f10f072a0522569a99bbd22bc00b8e50
Shailendra Singh <shailendra.singh@mechcloud.io>
2024-04-21 10:46:40 +0530

    Added code to dynamically append preflight css.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 export { default as CustomComponentsPlugin } from './plugins/RktVueComponentsPlugin'
 
+const linkTag = document.getElementById('preflight-css');
+if (linkTag) {
+} else {
+    const newLink1 = document.createElement('link');
+    newLink1.id = 'preflight-css'
+    newLink1.rel = 'stylesheet';
+    newLink1.href = 'https://unpkg.com/tailwindcss@3.4.3/src/css/preflight.css';
+
+    const firstStyleTag = document.querySelector('head style');
+    if (firstStyleTag) { 
+        document.head.insertBefore(newLink1, firstStyleTag);
+    } else {
+        // Fallback if no <style> tag is found 
+        document.head.appendChild(newLink1); 
+    }
+}
+
