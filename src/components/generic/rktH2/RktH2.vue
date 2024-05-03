3481b22dd539f9b18306e5f630ed422c9fce4e5f
Shailendra Singh <shailendra.singh@mechcloud.io>
2024-05-04 00:21:24 +0530

    Migrated RktH2 to edittext component.

--- src/components/generic/rktH2/RktH2.vue
+++ src/components/generic/rktH2/RktH2.vue
@@ -1,14 +1,19 @@
 <template>
-   <h2 v-if="isEditMode" v-model-text:msg="msg"></h2>
-   <h2 v-else v-text="msg"></h2>
+   <!-- <h2 v-if="isEditMode" v-model-text:msg="msg"></h2>
+   <h2 v-else v-text="msg"></h2> -->
+   <edit-text
+      tag="h2"
+      arg="msg"
+      :mapping="msg"
+   />
 </template>
 
 <script setup>
-   import { inject } from 'vue';
+   import {
+      EditText
+   } from '@mechcloud/piston-ui-sdk'
    
    defineProps({
       msg: String
    })
-
-   const isEditMode = inject('isEditMode')
 </script>
