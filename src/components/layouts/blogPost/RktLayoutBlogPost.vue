1eb706d27c4332dfeade66017acd1e9e7f76fc07
Shailendra Singh <shailendra.singh@mechcloud.io>
2024-09-18 19:18:34 +0530

    Fix for left navigation in blog layout.

--- src/components/layouts/blogPost/RktLayoutBlogPost.vue
+++ src/components/layouts/blogPost/RktLayoutBlogPost.vue
@@ -3,16 +3,16 @@
       mc-role="layout-blog-post"
    >
       <div class="navigation">
-         <h3>Series Posts</h3>
+         <h3>All Posts in Series</h3>
          <ul>
             <li 
-               v-for="siteNode in renderNode.children.filter(siteNode => !siteNode.hidden)"
+               v-for="siteNode in parentSiteNode.children.filter(siteNode => !siteNode.hidden)"
                :key="siteNode.id"
                class="tw1-border1"
             >
                <mc-navigation-link
                   :title="siteNode.attrs.title"
-                  :uriPrefix="uriPrefix"
+                  :uriPrefix="'/' + siteNode.attrs.prefix"
                   :mapping="siteNode.attrs.mapping"
                   :class="{ selected: siteNode.id === targetSiteNode.id }"
                />
@@ -45,7 +45,9 @@ import {
    McRenderNode, 
    McNavigationLink,
    mcUseNavigationStore,
-   mcUseGlobalCmpntUtils
+   mcUseGlobalCmpntUtils,
+   mcGetUriTokens1,
+   mcGetContext
 } from '@mechcloud/piston-ui-sdk'
 
 const { mcCmpntName } = mcUseGlobalCmpntUtils()
@@ -56,9 +58,17 @@ mcLog(logPrefix, `Entering setup() ..`)
 const mcNavigationStore = mcUseNavigationStore(window.pinia)
 const targetSiteNode = mcNavigationStore.getSiteNode()
 
-const renderNode = inject('render-node')
-const uriPrefix = inject('uriPrefix')
-mcLog(logPrefix, `Uri prefix : ${uriPrefix}`)
+const mcContext = mcGetContext()
+
+// const renderNode = inject('render-node')
+// const uriPrefix = inject('uriPrefix')
+// mcLog(logPrefix, `Uri prefix : ${uriPrefix}`)
+
+const uriTokens = mcGetUriTokens1()
+uriTokens.pop()
+const parentUri = uriTokens.join('/')
+mcLog(logPrefix, `Parent uri : ${parentUri}`)
+const parentSiteNode = mcContext.siteNodesIndex[parentUri]
 
 const expandedItemIds = ref([])
 provide('expandedItemIds', expandedItemIds)
@@ -83,11 +93,19 @@ div[mc-role="layout-blog-post"] {
    height: calc(100vh - 4.25rem);
    overflow: hidden; /* Prevent scrolling on the layout root */
    /* background-color: rgb(222, 228, 222); */
+   box-shadow: 0 2px 5px rgba(0,0,0,0.1);
 
    & > .navigation {
       padding: 1rem;
       box-shadow: 5px 0 5px -5px gray;
       overflow-y: auto; /* Allow vertical scrolling if needed */
+
+      & > h3 {
+         font-size: 1.5rem;
+         font-weight: 700;
+         margin-bottom: 0.75rem;
+         color: hsl(var(--muted-foreground));
+      }
 
       & li {
          font-size: 0.85rem;
