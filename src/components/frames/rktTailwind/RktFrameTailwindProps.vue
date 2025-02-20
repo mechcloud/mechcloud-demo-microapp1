7c2d526b57bd7db92c1e0d4286cbe56fdaee3f82
Shailendra Singh <shailendra.singh@mechcloud.io>
2025-02-21 00:41:15 +0530

    Removed Save button from RktFrameTailwindProps component.

--- src/components/frames/rktTailwind/RktFrameTailwindProps.vue
+++ src/components/frames/rktTailwind/RktFrameTailwindProps.vue
@@ -6,41 +6,24 @@
          label="Title"
          v-model="settings.title"
       />
-
-      <mc-button
-         label="Save"
-         style="width: max-content"
-         @click="pageDesignerEvent = { eventName: 'update-frame-props', eventData: settings }"
-      />
    </div>
 </template>
 
 <script setup>
    import { mcClone } from '@mechcloud/shared-js'
-   import { mcUseGlobalCmpntUtils } from '@mechcloud/piston-ui-sdk'
    import { 
       reactive,
       inject,
-      onMounted,
       watch
    } from 'vue'
 
-   const { mcContext } = mcUseGlobalCmpntUtils()
-
    defineEmits(['update-frame-props'])
 
-   // const selectedControl = inject('selectedControl')
-
    const frameVars = inject('frameVars')
-   const pageDesignerEvent = inject('pageDesignerEvent')
 
    const settings = reactive(
                      mcClone(frameVars.value)
                   )
-
-   // onMounted(() => {
-
-   // })
 
    watch(
       settings,
@@ -51,6 +34,5 @@
          deep: true
       }
    )
-
 </script>
 
