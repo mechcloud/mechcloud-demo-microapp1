6aa4c0a36e51ab23144829464c18a4e51cd9ff87
Shailendra Singh <shailendra.singh@mechcloud.io>
2025-05-02 00:39:02 +0530

    Misc changes.

---
+++ src/components/frames/rktTailwind/RktFrameTailwindProps1.vue
@@ -0,0 +1,33 @@
+<template>
+   <div 
+      class="mc-controls"
+   >
+      <mc-select
+         label="Theme"
+         :options="themes"
+         v-model="theme"
+      >
+      </mc-select>
+   </div>
+</template>
+
+<script setup>
+   import { 
+      inject,
+      onMounted
+   } from 'vue'
+
+   const theme = inject('theme')
+
+   const themes = [
+      {
+         id: 'tw1-theme-black',
+         label: 'Black'
+      },
+      {
+         id: 'tw1-theme-blue',
+         label: 'Blue'
+      }
+   ]
+</script>
+
